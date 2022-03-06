dc8776a64ef9715f474ea17c58b39330b11afb46
lebrancconvas <lebrancconvas@gmail.com>
2022-03-07 00:34:16 +0700

    ✨ Composite Pattern Implementation.

--- Lab07/src/Builder.java
+++ Lab07/src/Builder.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 class GraphicComposite
 {
 	public String name;
 	public String color;
 	public int width;
 	public int height;
+	public ArrayList<GraphicComposite> children = new ArrayList<>();
 
 	public GraphicComposite()
 	{
@@ -13,15 +16,32 @@ class GraphicComposite
 
 class Oval extends GraphicComposite
 {
-
+	public Oval(String color, int width, int height)
+	{
+		name = "Oval";
+		this.color = color;
+		this.width = width;
+		this.height = height;
+	}
 }
 
 class Rectangle extends GraphicComposite
 {
-
+	public Rectangle(String color, int width, int height)
+	{
+		name = "Rectangle";
+		this.color = color;
+		this.width = width;
+		this.height = height;
+	}
 }
 
 public class Builder 
 {
-	
+	public static void main(String[] args)
+	{
+		GraphicComposite drawing = new GraphicComposite();
+		drawing.children.add(new Rectangle("Red", 100, 100));
+		System.out.println(drawing);
+	}
 }
