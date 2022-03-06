c52079f74210b1ad4b9921a17643ab07f0bfd63c
lebrancconvas <lebrancconvas@gmail.com>
2022-03-07 00:20:42 +0700

    🔨 Create GraphicComposite Class and Child Classes.

--- Lab07/src/Builder.java
+++ Lab07/src/Builder.java
@@ -1,3 +1,26 @@
+class GraphicComposite
+{
+	public String name;
+	public String color;
+	public int width;
+	public int height;
+
+	public GraphicComposite()
+	{
+
+	}
+}
+
+class Oval extends GraphicComposite
+{
+
+}
+
+class Rectangle extends GraphicComposite
+{
+
+}
+
 public class Builder 
 {
 	
